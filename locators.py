60703c04c46ff900ade5d13890a4b5b200ea305d
pf13 <30930081+pf13@users.noreply.github.com>
2017-10-06 08:19:57 +0300

    Update locators.py

--- locators.py
+++ locators.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+
 from datafile import *
 
 class HhPage:
 	search_field = '[data-qa="vacancy-serp__query"]'
 	vacancies_group_title_link = '[data-qa="vacancies-group-title-link"]'
 	vacancy_serp_vacancy_title = '[data-qa="vacancy-serp__vacancy-title"]'
-	quantity_vacancies_on_page = '[.b-employerpage-vacancies.g-expand .b-employerpage-vacancies-region .b-employerpage-vacancies-hint]'
+	quantity_vacancies_on_page = '.b-employerpage-vacancies.g-expand .b-employerpage-vacancies-region .b-employerpage-vacancies-hint'
+	find_vacancy_title_in_result = '//a[contains(@href, employer_path)]'
